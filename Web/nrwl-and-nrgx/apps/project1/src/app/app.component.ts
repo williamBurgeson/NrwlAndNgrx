fec9d14c37aef29dcf8974251a93061f00127658
William Burgeson <William.Burgeson@googlemail.com>
2021-03-07 16:53:04 +0000

    Changed nrwl Project1 to visually match .net Project1

--- Web/nrwl-and-nrgx/apps/project1/src/app/app.component.ts
+++ Web/nrwl-and-nrgx/apps/project1/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nrwl-and-nrgx-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'project1';
+  title = 'app';
 }
