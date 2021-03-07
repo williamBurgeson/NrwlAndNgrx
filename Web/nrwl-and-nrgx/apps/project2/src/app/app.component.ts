3be78eea7222efbe0056fdd25e5cfff6717de1a6
William Burgeson <William.Burgeson@googlemail.com>
2021-03-07 16:21:00 +0000

    Added project1 and project2 to workspace

---
+++ Web/nrwl-and-nrgx/apps/project2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-and-nrgx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'project2';
+}
--- Web/nrwl-and-nrgx/jest.config.js
+++ Web/nrwl-and-nrgx/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/nrwl-and-ngrx'],
+  projects: [
+    '<rootDir>/apps/nrwl-and-ngrx',
+    '<rootDir>/apps/project1',
+    '<rootDir>/apps/project2',
+  ],
 };
